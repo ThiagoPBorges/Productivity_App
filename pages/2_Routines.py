07e94c4e5d8dc74742537905be1e0e361317d19b
Thiago <thiagoprochnow1@gmail.com>
2026-01-15 23:21:39 -0300

    feat: start to create books menu, dataframe and sidebar

--- pages/2_Routines.py
+++ pages/2_Routines.py
@@ -8,7 +8,32 @@ st.set_page_config(
     layout="wide"
 
 )
+# Load data
+df_books = get_df("books_library_d")
+
+# --- SIDEBAR ---
+
+st.sidebar.header("Navigation")
+
+if not df_books.empty:
+
+    unique_status = list(df_books["Status"].unique())
+    status_all = ["All"] + unique_status
+
+    selected_status = st.sidebar.selectbox("Status", status_all)
+
+    if selected_status != "All":
+        df_books = df_books[df_books["Status"] == selected_status]
+
+else:
+    st.warning("Any book found at library.")
+
 
 st.write("📚 My books library")
-df_books = get_df("books_library_d")
-st.dataframe(df_books)
+
+st.dataframe(
+    df_books,
+    use_container_width=True,
+    hide_index=True,
+    column_config={"ID_Google": None}
+)
